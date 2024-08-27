e919740e135046baa77f70fcdb569d926df20ac6
saulr9 <saulr9@users.noreply.github.com>
2024-08-27 04:45:09 +0000

    deploy: 31e0e6d5b69c4654f634167788c0ed97ece4fb25

---
+++ _next/static/chunks/app/layout-024bb70d03a9d595.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9355:function(n,e,u){Promise.resolve().then(u.t.bind(u,8877,23))},8877:function(){}},function(n){n.O(0,[404,971,165,744],function(){return n(n.s=9355)}),_N_E=n.O()}]);
